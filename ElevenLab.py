532e45a34cbb6f73c07f3284300c69185ea3dc3c
Bilal Khan <bilalkhan31c7@gmail.com>
2024-02-29 23:40:17 +0500

    final commit

--- ElevenLab.py
+++ ElevenLab.py
@@ -12,7 +12,7 @@ set_api_key("1656cb2a5373f912ad1782b5a4ce7113")
 
 voice = clone(
    name = "My Cloned Voice",
-   files = ['Videos/extracted_audio.wav']
+   files = ['Audios/extracted_audio.wav']
 )
 
 
@@ -26,7 +26,7 @@ play(audio, notebook=True)
 #Save audio
 # Assuming "audio" is a byte object containing the audio data
 
-output_path = "Videos/clone_vic.wav"
+output_path = "Audios/clone_vic.wav"
 
 with open(output_path, "wb") as f:
     f.write(audio)
